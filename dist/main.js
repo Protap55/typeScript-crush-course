653896fdd406c577289e9fc8068b554410c45552
protap55 <protapdutta500@gmail.com>
2026-09-23 12:14:38 +0600

    first commit

---
+++ dist/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// const person = (name: string, age: number): string => {
+//   return `Hi ${name}. Your age ${age} years old.`;
+// };
+Object.defineProperty(exports, "__esModule", { value: true });
+const studentInfo = {
+    name: "Protap dutta",
+    age: 28,
+    address: "Dhaka",
+    isJobsHolder: false,
+    qualification: "Civil engineering",
+    status: "single",
+};
+studentInfo.age = 29;
+console.log(studentInfo);
+//# sourceMappingURL=main.js.map
