5c932f6bb87758505aaacc54f8fe3cdc6cf84660
protap55 <protapdutta500@gmail.com>
2026-09-25 14:11:21 +0600

    polish this project

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,11 @@
-"use strict";
 // const person = (name: string, age: number): string => {
 //   return `Hi ${name}. Your age ${age} years old.`;
 // };
-Object.defineProperty(exports, "__esModule", { value: true });
 class student {
     nam = "Protap";
     age = 28;
 }
 const sg = new student();
 console.log(sg);
+export {};
 //# sourceMappingURL=main.js.map
